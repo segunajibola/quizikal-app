36dcb449c3d553b9a7327f28593f496ddcc384ad
Segun Ajibola <ajbl.segun@gmail.com>
2022-07-13 10:20:05 +0100

    removeForm

--- script.js
+++ script.js
@@ -14,134 +14,134 @@ const quizData = [
         d: "Cascading Stress Sheet",
         correct: "b"
     }
-    //, {
-    //     question: "3. Which element is used to display the title of a webpage?",
-    //     a: "Title Element",
-    //     b: "Theme Element",
-    //     c: "Topbar Element",
-    //     d: "Text Element",
-    //     correct: "a"
-    // }
-    //, {
-    //     question: "4. We have two types of list element",
-    //     a: "Arrange list & Scattered list",
-    //     b: "Number list & Bullet list",
-    //     c: "Form list & Table list",
-    //     d: "Ordered list & Unordered list",
-    //     correct: "d"
-    // }, {
-    //     question: "5. We have these set of heading element",
-    //     a: "h1 - h8",
-    //     b: "h2 - h6",
-    //     c: "h1 - h10",
-    //     d: "h1 - h6",
-    //     correct: "d"
-    // }, {
-    //     question: "6. Which element is used to create paragragh?",
-    //     a: "l element",
-    //     b: "m element",
-    //     c: "o element",
-    //     d: "p element",
-    //     correct: "d"
-    // }, {
-    //     question: "7. Which type of bracket is used in HTML?",
-    //     a: "<>",
-    //     b: "[]",
-    //     c: "{}",
-    //     d: "()",
-    //     correct: "a"
-    // }, {
-    //     question: "8. Which type of bracket is used in CSS?",
-    //     a: "<>",
-    //     b: "[]",
-    //     c: "{}",
-    //     d: "()",
-    //     correct: "c"
-    // }, {
-    //     question: "9. Which element creates images in our website?",
-    //     a: "image element",
-    //     b: "img element",
-    //     c: "i element",
-    //     d: "images element",
-    //     correct: "b"
-    // }, {
-    //     question: "10. HTML attributes are found in the opening tag",
-    //     a: "True",
-    //     b: "False",
-    //     c: "Not sure",
-    //     d: "It depends",
-    //     correct: "a"
-    // }, {
-    //     question: "11. Which of these elements does not have closing tag?",
-    //     a: "img element",
-    //     b: "h  element",
-    //     c: "p element",
-    //     d: "form element",
-    //     correct: "a"
-    // }, {
-    //     question: "12. The table element contains which set of elements?",
-    //     a: "tr, td, tf",
-    //     b: "tg, tt, ty",
-    //     c: "tr, th, td",
-    //     d: "tw, tm, tp",
-    //     correct: "c"
-    // }, {
-    //     question: "13. The form element contains which set of elements?",
-    //     a: "label & input elements",
-    //     b: "ul & ol elements",
-    //     c: "p & h elements",
-    //     d: "label & title elements",
-    //     correct: "a"
-    // }, {
-    //     question: "14. We use HTML, CSS & JavaScript for?",
-    //     a: "Structure, Style & Editing",
-    //     b: "Street, Style & Functionality",
-    //     c: "Structure, Style & Functionality",
-    //     d: "Style, Struce & Graphics",
-    //     correct: "c"
-    // }, {
-    //     question: "15. The <br> elements is used for what?",
-    //     a: "To go to a new topic",
-    //     b: "To go to a new line",
-    //     c: "To make our form beautiful",
-    //     d: "To make the label & input big",
-    //     correct: "b"
-    // }, {
-    //     question: "16. Padding is used for?",
-    //     a: "Inside space",
-    //     b: "Outside space",
-    //     c: "Middle space",
-    //     d: "HTML space",
-    //     correct: "a"
-    // }, {
-    //     question: "17. Margin is used for?",
-    //     a: "Inside space",
-    //     b: "Outside space",
-    //     c: "Middle space",
-    //     d: "HTML space",
-    //     correct: "b"
-    // }, {
-    //     question: "18. Which CSS property is used to increase the size of text?",
-    //     a: "text-size",
-    //     b: "word-size",
-    //     c: "font-size",
-    //     d: "increase-size",
-    //     correct: "c"
-    // }, {
-    //     question: "19. Which element is used to create the navigation bar on top of every page?",
-    //     a: "nal element",
-    //     b: "nav element",
-    //     c: "power element",
-    //     d: "form element",
-    //     correct: "b"
-    // }, {
-    //     question: "20. How do you hover on a text?",
-    //     a: "By putting CSS on the text",
-    //     b: "By putting your cursor(mouse) on the text",
-    //     c: "By changing the color",
-    //     d: "By using JavaScript",
-    //     correct: "b"
-    // }
+    , {
+        question: "3. Which element is used to display the title of a webpage?",
+        a: "Title Element",
+        b: "Theme Element",
+        c: "Topbar Element",
+        d: "Text Element",
+        correct: "a"
+    }
+    , {
+        question: "4. We have two types of list element",
+        a: "Arrange list & Scattered list",
+        b: "Number list & Bullet list",
+        c: "Form list & Table list",
+        d: "Ordered list & Unordered list",
+        correct: "d"
+    }, {
+        question: "5. We have these set of heading element",
+        a: "h1 - h8",
+        b: "h2 - h6",
+        c: "h1 - h10",
+        d: "h1 - h6",
+        correct: "d"
+    }, {
+        question: "6. Which element is used to create paragragh?",
+        a: "l element",
+        b: "m element",
+        c: "o element",
+        d: "p element",
+        correct: "d"
+    }, {
+        question: "7. Which type of bracket is used in HTML?",
+        a: "<>",
+        b: "[]",
+        c: "{}",
+        d: "()",
+        correct: "a"
+    }, {
+        question: "8. Which type of bracket is used in CSS?",
+        a: "<>",
+        b: "[]",
+        c: "{}",
+        d: "()",
+        correct: "c"
+    }, {
+        question: "9. Which element creates images in our website?",
+        a: "image element",
+        b: "img element",
+        c: "i element",
+        d: "images element",
+        correct: "b"
+    }, {
+        question: "10. HTML attributes are found in the opening tag",
+        a: "True",
+        b: "False",
+        c: "Not sure",
+        d: "It depends",
+        correct: "a"
+    }, {
+        question: "11. Which of these elements does not have closing tag?",
+        a: "img element",
+        b: "h  element",
+        c: "p element",
+        d: "form element",
+        correct: "a"
+    }, {
+        question: "12. The table element contains which set of elements?",
+        a: "tr, td, tf",
+        b: "tg, tt, ty",
+        c: "tr, th, td",
+        d: "tw, tm, tp",
+        correct: "c"
+    }, {
+        question: "13. The form element contains which set of elements?",
+        a: "label & input elements",
+        b: "ul & ol elements",
+        c: "p & h elements",
+        d: "label & title elements",
+        correct: "a"
+    }, {
+        question: "14. We use HTML, CSS & JavaScript for?",
+        a: "Structure, Style & Editing",
+        b: "Street, Style & Functionality",
+        c: "Structure, Style & Functionality",
+        d: "Style, Struce & Graphics",
+        correct: "c"
+    }, {
+        question: "15. The <br> elements is used for what?",
+        a: "To go to a new topic",
+        b: "To go to a new line",
+        c: "To make our form beautiful",
+        d: "To make the label & input big",
+        correct: "b"
+    }, {
+        question: "16. Padding is used for?",
+        a: "Inside space",
+        b: "Outside space",
+        c: "Middle space",
+        d: "HTML space",
+        correct: "a"
+    }, {
+        question: "17. Margin is used for?",
+        a: "Inside space",
+        b: "Outside space",
+        c: "Middle space",
+        d: "HTML space",
+        correct: "b"
+    }, {
+        question: "18. Which CSS property is used to increase the size of text?",
+        a: "text-size",
+        b: "word-size",
+        c: "font-size",
+        d: "increase-size",
+        correct: "c"
+    }, {
+        question: "19. Which element is used to create the navigation bar on top of every page?",
+        a: "nal element",
+        b: "nav element",
+        c: "power element",
+        d: "form element",
+        correct: "b"
+    }, {
+        question: "20. How do you hover on a text?",
+        a: "By putting CSS on the text",
+        b: "By putting your cursor(mouse) on the text",
+        c: "By changing the color",
+        d: "By using JavaScript",
+        correct: "b"
+    }
 ];
 
 const quiz = document.getElementById("quiz");
@@ -209,17 +209,13 @@ submitBtn.addEventListener("click", () => {
         } else {
                 quiz.innerHTML = `
                 <h2>You answered correctly ${score}/${quizData.length} questions.</h2>
-                <form action="form.php" method="post">
-                    <label for="name" class="submit_label">Fullname</label>
-                    <input type="text" id="name" name="name" class="submit_input"> <br><br>
-                    <label for="score" class="submit_score">Score</label>
-                    <input type="text" id="score" class="submit_score" name="score"> <br> <br>
-                    <label>
-                        <input type="checkbox" name="terms">
-                        I haven't cheated in any way.
-                    </label>
-                    <button>Send score</button>
-                </form>
+                <!-- <form action="form.php" method="post">
+                <label for="name" class="submit_label">Fullname</label>
+                <input type="text" id="name" name="name" class="submit_input"> <br><br>
+                <label for="score" class="submit_score">Score</label>
+                <input type="text" id="score" name="" class="submit_score" value="${score}" name="score" readonly> <br> <br>
+                <button>Send score</button>
+                </form>  -->
 
                 <button onclick="location.reload()">Restart quiz</button>
                 `;
